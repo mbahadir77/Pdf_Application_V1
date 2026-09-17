a1b7721aede7b6615d8f9f256daaa5e4ea5bcaf7
mbahadir77 <bhdryldrm77@hotmail.com>
2026-09-17 07:08:02 +0300

    eni logolar eklendi, kopya kaynaklar silindi, Gradle keystore hatası çözüldü.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
-  signingConfigs {
+  /*signingConfigs {
     create("release") {
       val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
       storeFile = file(keystorePath)
@@ -37,16 +37,16 @@ android {
       keyAlias = "androiddebugkey"
       keyPassword = "android"
     }
-  }
+  }*/
 
   buildTypes {
     release {
       isCrunchPngs = false
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
+
+      // signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
